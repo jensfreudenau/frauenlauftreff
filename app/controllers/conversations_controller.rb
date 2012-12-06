a0988da5f8fba533ed442a343825feaabb1de1f6
Jens Freudenau <jens@freude-now.de>
2012-12-06 15:00:20 +0100

    conversation
    Signed-off-by: Jens Freudenau <jens@freude-now.de>

---
+++ app/controllers/conversations_controller.rb
@@ -0,0 +1,90 @@
+class ConversationsController < ApplicationController
+  before_filter :authenticate_user!
+  helper_method :mailbox, :conversation
+  skip_before_filter :verify_authenticity_token, :only => [:untrash, :trash]
+
+  def index
+
+    puts '######@messages_count########' + current_user.mailbox.inbox({:read => false}).count.to_s
+  end
+
+  def create
+    recipient_ids = params['recipients']
+    recipients    = User.where(id: recipient_ids).all
+    conversation  = current_user.send_message(recipients, *conversation_params(:body, :subject)).conversation
+
+    redirect_to conversation
+  end
+
+  def reply
+    current_user.reply_to_conversation(conversation, *message_params(:body, :subject))
+    redirect_to conversation
+  end
+
+  def trash
+    conversation.move_to_trash(current_user)
+    redirect_to :conversations
+  end
+
+  def show
+    conversation.mark_as_read(current_user)
+  end
+
+  def untrash
+    conversation.untrash(current_user)
+    redirect_to :conversations
+  end
+
+  def destroy
+
+    conversation.move_to_trash(current_user)
+
+    respond_to do |format|
+      format.html {
+        if params[:location].present? and params[:location] == 'conversation'
+          redirect_to conversations_path(:box => :trash)
+        else
+          redirect_to conversations_path(:box => @box, :page => params[:page])
+        end
+      }
+      format.js {
+        if params[:location].present? and params[:location] == 'conversation'
+          render :js => "window.location = '#{conversations_path(:box => @box, :page => params[:page])}';"
+        else
+          render 'conversations/destroy'
+        end
+      }
+    end
+  end
+
+  private
+
+  def mailbox
+    @mailbox ||= current_user.mailbox
+  end
+
+  def conversation
+    @conversation ||= mailbox.conversations.find(params[:id])
+  end
+
+  def conversation_params(*keys)
+    fetch_params(:conversation, *keys)
+  end
+
+  def message_params(*keys)
+    fetch_params(:message, *keys)
+  end
+
+  def fetch_params(key, *subkeys)
+    params[key].instance_eval do
+      case subkeys.size
+        when 0 then
+          self
+        when 1 then
+          self[subkeys.first]
+        else
+          subkeys.map { |k| self[k] }
+      end
+    end
+  end
+end
