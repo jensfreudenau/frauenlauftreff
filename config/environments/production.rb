6f1883dbfb5be3684c78c74e273a25329b84674e
Jens Freudenau <jens@freude-now.de>
2013-03-31 20:26:11 +0200

    gmail 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,5 +69,6 @@ Frauenlauftreff::Application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+  ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
 
 end
