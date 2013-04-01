2e28b15d437de25ae829322bfe9348617563d451
Jens Freudenau <jens@freude-now.de>
2013-04-01 13:27:39 +0200

    change host
    Signed-off-by: Jens Freudenau <jens@freude-now.de>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Frauenlauftreff::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = {:host => 'http://frauenlauftreff.herokuapp.com'}
+  config.action_mailer.default_url_options = {:host => 'frauenlauftreff.herokuapp.com'}
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
